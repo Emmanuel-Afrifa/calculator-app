93961a16e15d406ac8c99d88e9502a94a144aeb1
EmmanuelAfrifa <emmanuel.afrifa@amalitech.org>
2023-09-08 19:11:19 +0000

    I updated th typescript file such that when a user selects a theme, the display changes accordingly. I also used mouse enter and mouseleave events to achieve the hover states styling

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 let calcHeading = document.querySelector('#calc-heading') as  HTMLHeadingElement;
 let themeName = document.querySelector('#theme-name') as HTMLHeadingElement;
 let themeLabel = document.querySelector('#theme-choice') as HTMLInputElement;
-let slider = document.querySelector('input[type="range"]') as HTMLInputElement;
+let choiceMarker = document.querySelector('#choice-marker') as HTMLLabelElement;
+let slider = document.querySelector('input') as HTMLInputElement;
 let resultDisplay = document.querySelector('#display-screen') as HTMLDivElement;
 let inputButtons = document.querySelector('#input-buttons') as HTMLDivElement;
 let numberOperators = document.querySelectorAll<HTMLButtonElement>('.number-operator');
@@ -9,21 +10,143 @@ let deleteButton = document.querySelector('#delete') as HTMLButtonElement;
 let resetButton = document.querySelector('#reset') as HTMLButtonElement;
 let equalButton = document.querySelector('#equal') as HTMLButtonElement;
 
+
+window.addEventListener('load', () => {
+    slider.value = '1'
+})
+
+/* Adding the mouseover event to the buttons depending on the theme chosen */
+// TOGGLE 
+slider.addEventListener('mouseenter', () => {
+    if (Number(slider.value) === 1){
+        slider.style.setProperty('--slider-thumb-bg-hover', 'hsl(6, 63%, 60%)');
+    }
+    if (Number(slider.value) === 2){
+        slider.style.setProperty('--slider-thumb-bg-hover', 'hsla(25, 98%, 60%)');
+    }
+    if (Number(slider.value) === 3){
+        slider.style.setProperty('--slider-thumb-bg-hover', 'hsla(176, 100%, 64%)');
+    }
+})
+
+slider.addEventListener('mouseleave', () => {
+    if (Number(slider.value) === 1){
+        slider.style.setProperty('--slider-thumb-bg-hover', 'hsl(6, 63%, 50%)');
+    }
+    if (Number(slider.value) === 2){
+        slider.style.setProperty('--slider-thumb-bg-hover', 'hsl(25, 98%, 40%)');
+    }
+    if (Number(slider.value) === 3){
+        slider.style.setProperty('--slider-thumb-bg-hover', 'hsl(176, 100%, 44%)');
+    }
+})
+
+
+// EQUAL BUTTON
+equalButton.addEventListener('mouseenter', () => {
+    hoverUpdateByTheme(equalButton, 'hsla(6, 63%, 60%)', 'hsla(25, 98%, 60%)', 'hsla(176, 100%, 64%)')
+})
+
+equalButton.addEventListener('mouseleave', () => {
+    hoverUpdateByTheme(equalButton, 'hsl(6, 63%, 50%)', 'hsl(25, 98%, 40%)', 'hsl(176, 100%, 44%)')
+})
+
+// DELETE AND RESET BUTTONS
+deleteButton.addEventListener('mouseenter', () => {
+    hoverUpdateByTheme(deleteButton, 'hsl(225, 21%, 69%)', 'hsl(185, 42%, 57%)', 'hsl(281, 89%, 45%)')
+})
+
+deleteButton.addEventListener('mouseleave', () => {
+    hoverUpdateByTheme(deleteButton, 'hsl(225, 21%, 49%)', 'hsl(185, 42%, 37%)', 'hsl(281, 89%, 26%)')
+})
+
+resetButton.addEventListener('mouseenter', () => {
+    hoverUpdateByTheme(resetButton, 'hsl(225, 21%, 69%)', 'hsl(185, 42%, 57%)', 'hsl(281, 89%, 45%)')
+})
+
+resetButton.addEventListener('mouseleave', () => {
+    hoverUpdateByTheme(resetButton, 'hsl(225, 21%, 49%)', 'hsl(185, 42%, 37%)', 'hsl(281, 89%, 26%)')
+})
+
+// NUMBER AND OPERATOR BUTTONS
+numberOperators.forEach((element: HTMLButtonElement) => {
+    element.addEventListener('mouseenter', () => { 
+        if (element.id === 'delete'){
+            return
+        }
+        if (Number(slider.value) === 1){
+            element.style.backgroundColor = 'hsl(0, 0%, 100%)'
+        }
+        if (Number(slider.value) === 2){
+            element.style.backgroundColor = 'hsl(0, 0%, 100%)'
+        }
+        if (Number(slider.value) === 3){
+            element.style.backgroundColor = 'hsl(268, 47%, 51%)'
+        }
+    })
+
+    element.addEventListener('mouseleave', () => { 
+        if (element.id === 'delete'){
+            return
+        }
+        if (Number(slider.value) === 1){
+            element.style.backgroundColor = 'hsl(30, 25%, 89%)'
+        }
+        if (Number(slider.value) === 2){
+            element.style.backgroundColor = 'hsl(45, 7%, 89%)'
+        }
+        if (Number(slider.value) === 3){
+            element.style.backgroundColor = 'hsl(268, 47%, 21%)'
+        }
+    })
+})
+
+
+// Adding an event handler function to the theme picker
+slider.addEventListener('click', () => {
+    // Theme 1
+    if (Number(slider.value) === 1){
+        document.querySelector('body')!.style.backgroundColor = 'hsl(222, 26%, 31%)';
+        topSectionUpdate('hsl(0, 0%, 100%)', 'hsl(6, 63%, 50%)', 'hsl(223, 31%, 20%)');
+        updateDisplayScreen('hsl(224, 36%, 15%)', 'hsl(0, 0%, 100%)');
+        updateInputButtons('hsl(223, 31%, 20%)', 'hsl(30, 25%, 89%)', 'hsl(28, 16%, 65%)', 'hsl(225, 21%, 49%)',
+        'hsl(224, 28%, 35%)', 'hsl(6, 63%, 50%)', 'hsl(6, 70%, 34%)', 'hsl(221, 14%, 31%)', 'hsl(0, 0%, 100%)');
+        equalButton.style.color = 'hsl(0, 0%, 100%)'
+    }
+
+    if (Number(slider.value) === 2){
+        document.querySelector('body')!.style.backgroundColor = 'hsl(0, 0%, 90%)';
+        topSectionUpdate('hsl(60, 10%, 19%)', 'hsl(25, 98%, 40%)', 'hsl(0, 5%, 81%)');
+        updateDisplayScreen('hsl(0, 0%, 93%)', 'hsl(60, 10%, 19%)');
+        updateInputButtons('hsl(0, 5%, 81%)', 'hsl(45, 7%, 89%)', 'hsl(35, 11%, 61%)', 'hsl(185, 42%, 37%)', 'hsl(185, 58%, 25%)', 
+        'hsl(25, 98%, 40%)', 'hsl(25, 99%, 27%)', 'hsl(60, 10%, 19%)', 'hsl(0, 0%, 100%)');
+        equalButton.style.color = 'hsl(0, 0%, 100%)'
+    }
+
+    if (Number(slider.value) === 3){
+        document.querySelector('body')!.style.backgroundColor = 'hsl(268, 75%, 9%)';
+        topSectionUpdate('hsl(52, 100%, 62%)', 'hsl(176, 100%, 44%)', 'hsl(268, 71%, 12%)');
+        updateDisplayScreen('hsl(268, 71%, 12%)', 'hsl(52, 100%, 62%)');
+        updateInputButtons('hsl(268, 71%, 12%)', 'hsl(268, 47%, 21%)', 'hsl(290, 70%, 36%)', 'hsl(281, 89%, 26%)', 'hsl(285, 91%, 52%)', 
+        'hsl(176, 100%, 44%)', 'hsl(177, 92%, 70%)', 'hsl(52, 100%, 62%)', 'hsl(0, 0%, 100%)')
+        equalButton.style.color = 'hsl(198, 20%, 13%)'
+    }
+})
+
+
 // A callback function that modifies the top section
-function topSectionUpdate(nameColor: string, themeNameColor: string, labelColor: string, thumbColor: string, sliderColor: string):void {
+function topSectionUpdate(nameColor: string, thumbColor: string, sliderColor: string):void {
     /**
      * This function updates the color or background color of the top-section.
      * @param nameColor: string -> new color for the main title
-     * @param themeNameColor: string -> new color for the theme name
-     * @param labelColor: string -> new color for the theme picker label
      * @param thumbColor: string -> new background color for the slider thumb
      * @param sliderColor: string -> new background color for the slider
      * @returns void
      */
 
     calcHeading.style.color = nameColor;
-    themeName.style.color = themeNameColor;
-    themeLabel.style.color = labelColor;
+    themeName.style.color = nameColor;
+    choiceMarker.style.color = nameColor;
     slider.style.backgroundColor = sliderColor;
     slider.style.setProperty('--slider-thumb-bg-color', thumbColor);
 }
@@ -73,4 +196,42 @@ function updateInputButtons(mainBGColor: string, numOperatorBG: string, numOpera
         resetButton.style.color = delTextColor;
         equalButton.style.backgroundColor = equalBG;
         equalButton.style.borderBottomColor = equalBorderBottom;
+}
+
+// A callback function changes the background color of the specified element
+function updateHoverStates(currentButton: HTMLButtonElement, bgColor: string):void {
+    /**
+     * A function that changes the background color of the current element
+     * @param currentButton: HTMLButtonElement -> the button element whose background color we wish to change
+     * @param bgColor: string -> the new background color to be applied
+     */
+
+    currentButton.style.backgroundColor = bgColor;
+}
+
+
+// A callback function for the hover states for the various themes
+function hoverUpdateByTheme(currentButton: HTMLButtonElement, themeOneValue: string, themeTwoValue: string, themeThreeValue: string): void {
+    /**
+     * This function changes the hover states depending on the current theme
+     * @param currentButton: HTMLButtonElement -> current element whose background we wish to change
+     * @param themeOneValue: string -> color for theme one
+     * @param themeTwoValue: string -> color for theme two
+     * @param themeThreeValue: string -> color for theme three
+     */
+
+    if (Number(slider.value) === 1){
+        // updateHoverStates(equalButton, 'hsla(6, 63%, 60%)');
+        updateHoverStates(currentButton, themeOneValue);
     }
+
+    if (Number(slider.value) === 2){
+        // updateHoverStates(equalButton, 'hsla(25, 98%, 60%)');
+        updateHoverStates(currentButton, themeTwoValue);
+    }
+
+    if (Number(slider.value) === 3){
+        // updateHoverStates(equalButton, 'hsla(176, 100%, 64%)');
+        updateHoverStates(currentButton, themeThreeValue);
+    }
+}
